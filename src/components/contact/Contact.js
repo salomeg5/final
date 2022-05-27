e7128e8499f1f697fe370e22cb0613ebfe98050a
salomeg5 <98424361+salomeg5@users.noreply.github.com>
2022-05-26 20:51:37 -0500

    form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -43,7 +43,22 @@ function Contact(){
             <hr/>
             <img src={firma} alt="firma"></img>
             <br></br>
-
+            <form action="/my-handling-form-page" method="post">
+ <ul>
+  <li>
+    <label for="name">Nombre:</label>
+    <input type="text" id="name" name="user_name" />
+  </li>
+  <li>
+    <label for="mail">Correo electrónico:</label>
+    <input type="email" id="mail" name="user_email" />
+  </li>
+  <li>
+    <label for="msg">Mensaje:</label>
+    <textarea id="msg" name="user_message"></textarea>
+  </li>
+</ul>
+</form>
             </div>
             </div>
         </div>
