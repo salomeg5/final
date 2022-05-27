a13eec3d93a2f01395838b1dda8962d4aaa30122
salomeg5 <98424361+salomeg5@users.noreply.github.com>
2022-05-26 20:41:22 -0500

    1

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -7,7 +7,7 @@ function Error404(){
 	return(
 	<div className="container">
          <img src="https://www.portafolio.co/files/article_main/uploads/2016/04/26/571fb7d8122ef.jpeg"/>
-		 <Link to="/" className="btn btn-danger">Click para Hoja de vida</Link>  
+         <Link to="/" className="btn btn-danger">Click para Hoja de vida</Link>  
       </div>
 		);
 }
